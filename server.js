43a30642f999da3b6c050395bf60f20ca8b56a5d
dextreza <gloyle@gmail.com>
2015-08-19 20:55:12 +0200

    logs para Logentries
    prueba de creacion de una tag

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ var servidor = http.createServer(function(req,res){
 
 }).listen(puerto);
 
-console.log('servidor funcionando en puerto:' + puerto);
+console.log('dextreza:: servidor funcionando en puerto:' + puerto);
 
 var io = require('socket.io').listen(servidor);
 
 io.sockets.on('connection',function(elSocket){
 
-	//console.log('El usuario '+ parameters.nombre +' se ha conectado al socket');
-	console.log('El usuario se ha conectado al socket');
+	//console.log('dextreza:: El usuario '+ parameters.nombre +' se ha conectado al socket');
+	console.log('dextreza:: El usuario se ha conectado al socket');
 
 	elSocket.emit('message',{text:'Te has conectado!'});//mensaje para el usuario
 
 	elSocket.on('respuesta',function(mensaje){
-		console.log('Respuesta de un cliente:' + mensaje.text);		
+		console.log('dextreza:: Respuesta de un cliente:' + mensaje.text);		
 	});
 
 	elSocket.on('nuevo',function(mensaje,funcionDelCliente){
-		console.log('Nuevo comentario de un cliente:' + mensaje.text);
+		console.log('dextreza:: Nuevo comentario de un cliente:' + mensaje.text);
 		//ahora ejecutamos una funcion en el cliente que nos ha pasado en la llamada
 		funcionDelCliente('Comentario recibido por el servidor');		
 		elSocket.broadcast.emit('comentario',{text:mensaje.text});//mensaje para todos lo asuario conectados (excepto a este mismo cliente) a este socket.El contenido del mensaje puede ser cualquier cosa.aqui es un objeto con una propiedad text		
@@ -35,7 +35,7 @@ io.sockets.on('connection',function(elSocket){
 	});
 
 	elSocket.on('disconnect',function(){
-		console.log('Usuario desconectado');
+		console.log('dextreza:: Usuario desconectado');
 		elSocket.broadcast.emit('anuncio',{text:'Un usuario se ha desconectado.'});
 	});
 });
